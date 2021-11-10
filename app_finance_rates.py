3b0154f4e2f56378c54e559e5964f911dc92f53c
martin-merener <martin.merener@gmail.com>
2021-11-10 00:16:22 -0500

    debugging

--- app_finance_rates.py
+++ app_finance_rates.py
@@ -81,6 +81,7 @@ try:
 			data.interpolate(inplace=True)
 		variables = list(set([p[0] for p in data.columns]))
 		symbols = list(set([p[1] for p in data.columns]))
+		st.write(symbols)
 		if ('Close' in variables) and ('Open' in variables):
 			cols_analyze = [(v,s) for v in ['Open', 'Close'] for s in symbols]
 			data_analyze = data[cols_analyze].copy()
