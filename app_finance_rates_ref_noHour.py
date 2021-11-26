f6314a4aa7913cc861355d82c1ffaa2b22e8f16d
martin-merener <martin.merener@gmail.com>
2021-11-26 15:43:10 -0500

    corrected wording:

--- app_finance_rates_ref_noHour.py
+++ app_finance_rates_ref_noHour.py
@@ -106,7 +106,7 @@ try:
 			change_rates['month'] = change_rates['month'].apply(lambda dt: datetime.datetime.strptime(str(dt)[0:10], "%Y-%m-%d").month)
 			change_rates['quarter'] = change_rates['month'].apply(lambda m: int(np.ceil(m/3)))				
 			if change_rates.shape[1]>0:
-				latex = r'''## Some insights on the price change (%), $r = \frac{S_{close}}{S_{open}}-1$'''
+				latex = r'''## Insights on % price change: $r = \frac{S_{close}}{S_{open}}-1$'''
 				st.markdown(latex)
 				for s in symbols:
 					st.markdown('''#### {0} - {1}:'''.format(s, dict(symbol_company_pairs)[s]))
